e6aa5b1004e6f9df0efd3488088aa6a3d00f6475
jahanabir8 <abir.opediatech@gmail.com>
2022-07-04 14:38:06 +0600

    crypto

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,11 +43,88 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+// review section here
+$('.your-class1').slick({
+    slidesToShow: 1,
+    centerMode:true,
+    fade: true,
+    slidesToScroll: 1,
+    margin:0,
+    arrows: false,
+  });
+  $('.your-class2').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    asNavFor: '.your-class1',
+    dots: false,
+    focusOnSelect: true
+  });
+
 
 // FAQ accordion
-const FAQ__accordion = document.getElementsByClassName('content__box');
-for(var i = 0; i<FAQ__accordion.length; i++){
-    FAQ__accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
-    })
-}
+// const FAQ__accordion = document.getElementsByClassName('content__box');
+// for(var i = 0; i<FAQ__accordion.length; i++){
+//     FAQ__accordion[i].addEventListener('click', function(){
+//         $('.content__box').removeClass('active')
+//         this.classList.addClass('active')
+//     })
+// }
+
+$('.content__box').on('click',function(e){
+    $('.content__box').removeClass('active')
+    $(this).addClass('active')
+})
+
+
+// piechart
+  // const labels = [
+  //   'January',
+  //   'February',
+  //   'March',
+  //   'April',
+  //   'May',
+  //   'June',
+  // ];
+
+//   const data = {
+//     labels: labels,
+//     datasets: [{
+//       label: 'My First dataset',
+//       backgroundColor: 'rgb(255, 99, 132)',
+//       borderColor: 'rgb(255, 99, 132)',
+//       data: [0, 10, 5, 2, 20, 30, 45],
+//     }]
+//   };
+
+const data = {
+    // labels: [
+    //   '21.51% Team',
+    //   '17.80% Investors',
+    //   '0.69% Advisors',
+    //   '60% Community'
+    // ],
+    datasets: [{
+      label: 'My First Dataset',
+    //   data: [300, 50, 100],
+    data: [96.795, 70.1, 73.105, 210],
+      backgroundColor: [
+        'rgba(37, 85, 254, 1)',
+        'rgba(108, 140, 252, 1)',
+        'rgba(156, 177, 252, 1)',
+        'rgba(0, 56, 255, 1)'
+      ],
+    //   hoverOffset: 2
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {}
+  };
+
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
